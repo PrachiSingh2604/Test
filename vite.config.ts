ea9964c120c24c099dbe123de9e25d33f1b5123c
PrachiSingh2604 <prachi.singh1624@gmail.com>
2025-05-07 14:28:45 +0530

    changes in vite

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
-export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/Test/' : '/', // 👈 Live vs Local
+/// https://vitejs.dev/config/
+export default defineConfig({
+  base: '/Test/', // 👈 ADD THIS LINE
   plugins: [react()],
-}))
+})
